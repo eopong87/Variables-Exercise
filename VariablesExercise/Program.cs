a7ae40cbce93131e81c28ca66c508cb739d64ba9
eopong87 <eopong87@gmail.com>
2023-06-16 12:11:47 -0400

    console

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string childsname; //Declaration syntax
+            childsname = "JON JON"; // Initialized--- assigning a value
+            int childsage = 5; // Declaring and intializing
+            char firstinitial = 'J';
+            bool ishungry = true;
+            double childsweight = 60.9;
+            decimal childsheight = 42.5m;
+            Console.WriteLine($"hello my childs name is {childsname} and he is {firstinitial}, and he is {childsage} years old.");
+            Console.WriteLine($"He weighs {childsweight} pounds and he is {childsheight}inches tall.");
+            Console.WriteLine($" He is {ishungry} he is always hungry.");
         }
     }
 }
